743073a54c5c46adcf4803e99b38688b6d73bdc9
ollihansen90 <ollihansen90@gmail.com>
2021-04-15 10:05:56 +0200

    forgot .to(device)

--- main.py
+++ main.py
@@ -9,21 +9,23 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 
 labellist = [3,4,7]
 #labellist = list(range(10))
-model = VisualTransformer(inner_dim=49*2, num_classes=len(labellist))
+model = VisualTransformer(inner_dim=49*2, num_classes=len(labellist)).to(device)
 dataset = MNIST_data(labels=labellist)
 dataloader = Dataloader(dataset, num_classes=len(labellist))
 
 lr = 1e-3
 betas = (0.95, 0.99)
-optimizer = torch.optim.Adam(model.parameters(), lr=lr, betas=betas)
+optimizer = torch.optim.Adam(model.parameters(), lr=lr, betas=betas).to(device)
 batch_size = 8
 
 n_epochs = 100
-lossliste = torch.zeros(n_epochs)
+lossliste = torch.zeros(n_epochs).to(device)
 
 for epoch in range(n_epochs):
     optimizer.zero_grad()
     img, label = dataloader.getbatch(n=batch_size)
+    img = img.to(device)
+    label = label.to(device)
     #print(img.shape)
     loss = torch.sum((label-model(img))**2)
     loss.backward()
@@ -33,5 +35,5 @@ for epoch in range(n_epochs):
         print(epoch, loss)
 
 plt.figure()
-plt.plot(lossliste, "b.")
+plt.plot(lossliste.cpu())
 plt.savefig("plots/plot_{}.png".format(dt.now().timestamp()))
